8681bdb0f2e0575b7a35323de50190a5a2567e23
pewpewmaster <113816932+pewpewmaster@users.noreply.github.com>
2022-10-26 22:11:51 -0600

    Add files via upload

---
+++ snake - Copy/snake-incomplete/snake/__main__.py
@@ -0,0 +1,45 @@
+#from snake-incomplete.snake.constants import Player_2
+#from snake-incomplete.snake.constants import Player_1
+import constants
+
+from game.casting.cast import Cast
+from game.casting.food import Food
+from game.casting.score import Score
+from game.casting.snake import Snake
+from game.scripting.script import Script
+from game.scripting.control_actors_action import ControlActorsAction
+from game.scripting.move_actors_action import MoveActorsAction
+from game.scripting.handle_collisions_action import HandleCollisionsAction
+from game.scripting.draw_actors_action import DrawActorsAction
+from game.directing.director import Director
+from game.services.keyboard_service import KeyboardService
+from game.services.video_service import VideoService
+from game.shared.color import Color
+from game.shared.point import Point
+from constants import MAX_X, MAX_Y, Player_1, Player_2
+
+def main():
+    
+    # create the cast
+    cast = Cast()
+    cast.add_actor("foods", Food())
+    cast.add_actor("snakes", Snake(Player_1))
+    cast.add_actor("snakes1", Snake(Player_2))
+    cast.add_actor("scores", Score())
+   
+    # start the game
+    keyboard_service = KeyboardService()
+    video_service = VideoService()
+
+    script = Script()
+    script.add_action("input", ControlActorsAction(keyboard_service))
+    script.add_action("update", MoveActorsAction())
+    script.add_action("update", HandleCollisionsAction())
+    script.add_action("output", DrawActorsAction(video_service))
+    
+    director = Director(video_service)
+    director.start_game(cast, script)
+
+
+if __name__ == "__main__":
+    main()
